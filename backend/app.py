0c739a6da6f7ca72de0a296981ac7daf276f31a2
Vitória Camara <120670231+iamvitoria@users.noreply.github.com>
2025-05-21 21:02:59 -0300

    fix: edit app.py

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 from db import get_connection
 import os
 import bcrypt
+from flask_cors import CORS
 
 app = Flask(__name__)
 CORS(app, supports_credentials=True)
+
+CORS(app, supports_credentials=True, resources={r"/*": {"origins": "*"}}, methods=["GET", "POST", "PUT", "DELETE", "OPTIONS"])
 
 @app.route("/test-db")
 def test_db():
